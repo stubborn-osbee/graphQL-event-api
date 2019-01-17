12d4831e83ab0f6d9a9c45b34f8ada4447f9e17b
unknown <osborne.saka@meltwater.org>
2019-01-17 00:15:29 +0000

    Changes set

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { BuildSchema } = require("graphql");
+
 const app = express();
 
 app.get(bodyParser().json);
 
-app.get("/graphql", graphqlHttp());
+app.get(
+  "/graphql",
+  graphqlHttp({
+    schema: BuildSchema(`
+         schema {
+             query:
+             mutation:
+         }
+        `),
+    rootValue: {}
+  })
+);
 
 app.listen(3000);
